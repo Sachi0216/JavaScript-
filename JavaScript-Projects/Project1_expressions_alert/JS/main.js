e2f7ef02f59a648d3172acf5588b8360c9c484d8
Sachi0216 <davis.elijah15@hotmail.com>
2022-01-05 18:48:18 -0600

    First js submission

---
+++ JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+
+window.alert('Hello, World');
+
+var = x"Elijah said, I want to be rich, Work for it God Said"
+
+document.write(x){
+("Elijah Said, \"I want to rich\"<br>
+\"Work for it\" God Replied");
+
+var b = "Con" + "string"
+document.write(b) { ("\"Be who you are and say how you fell,"
++"because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Seuss");
+}
+
+var blues = "I have the blues.");
+	var blues = blues.fontcolor("blue");
+	document.write(blues);
+	
+function My_first_Function() {
+		var str = "This text is green";
+		var result = str.fontcolor("green");
+		document.getElementById("green_Text").innerHTMl = result;
+		}
