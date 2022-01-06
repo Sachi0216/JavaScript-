09ef1a1e5f4a7afefbb20a919a0a47a8acd034e0
Sachi0216 <davis.elijah15@hotmail.com>
2022-01-05 19:47:09 -0600

    Update main.js

--- JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,24 +1,21 @@
 
 window.alert('Hello, World');
 
-var = x"Elijah said, I want to be rich, Work for it God Said"
+var x="Elijah said, I want to be rich, Work for it God Said"
 
-document.write(x){
-("Elijah Said, \"I want to rich\"<br>
-\"Work for it\" God Replied");
+document.write(x)
 
 var b = "Con" + "string"
-document.write(b) { ("\"Be who you are and say how you fell,"
+document.write("\"Be who you are and say how you fell,"
 +"because those who mind don\'t matter and those who matter don\'t mind.\""
 +"-Dr. Seuss");
-}
 
-var blues = "I have the blues.");
+var blues = "I have the blues.";
 	var blues = blues.fontcolor("blue");
 	document.write(blues);
 	
-function My_first_Function() {
+function My_First_Function() {
 		var str = "This text is green";
 		var result = str.fontcolor("green");
-		document.getElementById("green_Text").innerHTMl = result;
+		document.getElementById("Green_Text").innerHTML = result;
 		}
