ba5de088cab20b3469edd1757817c659900dd3b6
Sachi0216 <davis.elijah15@hotmail.com>
2022-01-05 19:13:21 -0600

    Project2

---
+++ JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function MyFunction() {
+    var sentence = "I am learning";
+    sentence +=" a lot from this course";
+    document.getElementById("Concatenate").innerHTMl = sentence;
+    }//targeting Id in html
+
