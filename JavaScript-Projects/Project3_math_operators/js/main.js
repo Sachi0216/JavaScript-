133856c47bf7b2df3d362726b83c35b0e48fa93e
Sachi0216 <davis.elijah15@hotmail.com>
2022-01-06 00:43:56 -0600

    Math

---
+++ JavaScript-Projects/Project3_math_operators/js/main.js
@@ -0,0 +1,10 @@
+
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML="5 - 2 = " + subtraction;
+}//subtraction in js
+
+function addition_Function() {
+    var subtraction = 5 + 2;
+    document.getElementById("Math").innerHTML="5 + 2 = " + subtraction;
+}//addition in js
