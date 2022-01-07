923579e3ca46fd6dce452f7f3b230d149346bd5c
Sachi0216 <davis.elijah15@hotmail.com>
2022-01-06 19:37:13 -0600

    fix

--- JavaScript-Projects/Project3_math_operators/js/main.js
+++ JavaScript-Projects/Project3_math_operators/js/main.js
@@ -6,5 +6,23 @@ function subtraction_Function() {
 
 function addition_Function() {
     var subtraction = 5 + 2;
-    document.getElementById("Math").innerHTML="5 + 2 = " + subtraction;
+    document.getElementById("addition").innerHTML="5 + 2 = " + subtraction;
 }//addition in js
+
+//multiplecation
+function multiplication_Function() {
+    var multiplication = 5 * 2;
+    document.getElementById("multiplication").innerHTML="5 * 2" + multiplication;
+}
+
+//division
+function division_Function() {
+    var division = 48 / 6;
+    document.getElementById("division").innerHTML ="48 / 6 =" + division;
+}
+
+//modulus operator
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("modulus").innerHTML ="When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
