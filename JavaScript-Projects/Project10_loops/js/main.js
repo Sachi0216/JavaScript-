5a759e134e14aaa0787571583b840467420c6aed
Sachi0216 <davis.elijah15@hotmail.com>
2022-01-07 13:19:13 -0600

    assignment

---
+++ JavaScript-Projects/Project10_loops/js/main.js
@@ -0,0 +1,38 @@
+function count_To_Ten() {
+    var Digit ="";
+    var X = 1;
+    while (x < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
+
+var Instruments =["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y <Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+//Array
+function cat_pics() {
+    var Cat_Picture =[];
+    Cat_Picture[0] ="sleeping";
+    Cat_Picture[1] ="playing";
+    Cat_Picture[2]="eating";
+    Cat_Picture[3]="purring";
+    document.getElementById("Cat").innerHTML = "in this picture, the cat is" +
+        Cat_Picture[2] +".";
+}
+
+//let
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
