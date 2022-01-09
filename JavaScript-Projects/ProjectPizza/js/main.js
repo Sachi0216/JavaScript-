f3a1569575318c971e028880fd34ac5ffd06ab6e
Sachi0216 <davis.elijah15@hotmail.com>
2022-01-08 21:35:04 -0600

    fix

--- JavaScript-Projects/ProjectPizza/js/main.js
+++ JavaScript-Projects/ProjectPizza/js/main.js
@@ -1,5 +1,5 @@
 function getReceipt() {
-    var text1 = "<h3> You Ordered:";
+    var text1 = "<h3> You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
@@ -9,9 +9,9 @@ function getReceipt() {
             text1 = text1+selectedSize+"<br>";
         }
     }
-    if (selectedSize=== "Personal Pizza") {
+    if (selectedSize === "Personal Pizza") {
         sizeTotal= 6;
-    } else if (selectedSize=== "Small Pizza") {
+    } else if (selectedSize === "Small Pizza") {
         sizeTotal = 8;
     }else if (selectedSize === "Medium Pizza") {
         sizeTotal = 10;
@@ -20,10 +20,10 @@ function getReceipt() {
     }else if (selectedSize === "Extra Large Pizza") {
         sizeTotal = 16;
     }
-}
 
-runningTotal=sizeTotal;
-console.log(selectedSize+"= $"+sizeTotal+".00");
+
+runningTotal = sizeTotal;
+console.log(selectedSize+" = $"+sizeTotal+".00");
 console.log("size text1: "+text1);
 console.log("subtotal: $"+runningTotal+".00");
 //these variables will get passed on to each function
@@ -31,27 +31,28 @@ getTopping(runningTotal,text1);
 };
 
 function getTopping(runningTotal,text1) {
-    var toppingTotal =0;
-    var selectedTopping=[];
+    var toppingTotal = 0;
+    var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
     for (var j = 0; j < toppingArray.length; j++) {
-        if (toppingArray[j].checked){
-            selectedTopping,push(toppingArray[j].value);
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value);
             console.log("selected topping item:("+toppingArray[j].value+")");
             text1 = text1+toppingArray[j].value+"<br>";
-                }
+         }
     }
     var toppingCount = selectedTopping.length;
-    if (toppingCount>1) {
+    if (toppingCount > 1) {
         toppingTotal = (toppingCount - 1);
     } else{
-        toppingTotal=0;
+        toppingTotal = 0;
     }
-    runningTotal=(runningTotal + toppingTotal);
+    runningTotal = (runningTotal + toppingTotal);
     console.log("total selected topping items: "+toppingCount);
     console.log(toppingCount+"topping - 1 free topping = "+"$"+toppingTotal+".00");
     console.log("topping text1: "+text1);
+    console.log("Purchase Total: "+"$"+runningTotal+".00");
     document.getElementById("showText").innerHTML=text1;
-    document.getElementById("totalPrice").innerHTML="<h3>Total: <strong>$+
+    document.getElementById("totalPrice").innerHTML="<h3>Total: <strong>$"+
         runningTotal+".00"+"</strong></h3>";
-}
+};
